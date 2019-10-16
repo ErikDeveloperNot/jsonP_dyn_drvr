2a7a8e8cf67324f26f7c7e99a069fbf9b3805fc4
ErikDeveloperNot <ErikDeveloperNot@gmail.com>
2019-10-16 02:11:20 -0700

    added test to use the buffer parser

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include "jsonP_parser.h"
+//#include "jsonP_parser.h"
+#include "jsonP_buffer_parser.h"
 
 #include <iostream>
 #include <iomanip>
-#include <fstream>
+//#include <fstream>
+#include <stdio.h>
 #include <chrono>
 
 
@@ -15,102 +17,121 @@ void parse_file(std::string, std::string &);
 
 int main(int argc, char **argv)
 {
-	// sample how to parse json text as array/object
-	std::string t{"{\n  \"key1\" : null, \n  \"key2\" : +35.544e4, \n  \"key3\" : 12345.321e+2, \n  \"obj\" : { \n    \"k1\" : \"v1\", \n  \"k2\" : \"v2\" \n  },\n  \"found\" : \"found value\", \n  \"arr\": [ null,\"ak1\" ,true, \"ak_val1\" ] \n }"};
-	std::string t2{"[{\"key1\" : null, \"key2\" : +35.544e4, \"key3\" : \"string value\", \"small_array\" : [true, false, true]}, {\"key1\" : null, \"key2\" : -452.544e-2, \"key3\" : \"string value2\", \"small_array\" : [true, false, false]}]"};
-	
-	element *doc_root = nullptr;
-	jsonP_doc *doc = nullptr;
-	
-	try {
-		jsonP_parser parser{};
-		doc = parser.parse(t);			//json text of type object
-//		doc = parser.parse(t2);		//json text of type array
-
-		std::string s;
-		std::string s2;
-
-		std::cout << std::boolalpha;
-		std::cout << "Doc Type: " << doc->get_type() << std::endl;
-
-		if (doc->get_type() == object) {
-			doc_root = doc->get_object();
-		} else if (doc->get_type() == array) {
-			doc_root = doc->get_array();
-		}
-		
-		doc_root->stringify(s);					//compact print
-		doc_root->stringify_pretty(s2);			//pretty print
-		
-		std::cout << "\n\n*** stringify ***\n" << s << std::endl;
-		std::cout << "\n\n*** pretty 1 ***\n" << s2 << std::endl;
-		
-		delete doc;
-		delete doc_root;
-
-	} catch (jsonP_exception &ex) {
-		std::cout << "parse exception caught: " << ex.what() << std::endl;
-		delete doc;
-		delete doc_root;
-	}
+//	// sample how to parse json text as array/object
+//	std::string t{"{\n  \"key1\" : null, \n  \"key2\" : +35.544e4, \n  \"key3\" : 12345.321e+2, \n  \"obj\" : { \n    \"k1\" : \"v1\", \n  \"k2\" : \"v2\" \n  },\n  \"found\" : \"found value\", \n  \"arr\": [ null,\"ak1\" ,true, \"ak_val1\" ] \n }"};
+//	std::string t2{"[{\"key1\" : null, \"key2\" : +35.544e4, \"key3\" : \"string value\", \"small_array\" : [true, false, true]}, {\"key1\" : null, \"key2\" : -452.544e-2, \"key3\" : \"string value2\", \"small_array\" : [true, false, false]}]"};
+//	
+//	element *doc_root = nullptr;
+//	jsonP_doc *doc = nullptr;
+//	
+//	try {
+//		jsonP_parser parser{};
+//		doc = parser.parse(t);			//json text of type object
+////		doc = parser.parse(t2);		//json text of type array
+//
+//		std::string s;
+//		std::string s2;
+//
+//		std::cout << std::boolalpha;
+//		std::cout << "Doc Type: " << doc->get_type() << std::endl;
+//
+//		if (doc->get_type() == object) {
+//			doc_root = doc->get_object();
+//		} else if (doc->get_type() == array) {
+//			doc_root = doc->get_array();
+//		}
+//		
+//		doc_root->stringify(s);					//compact print
+//		doc_root->stringify_pretty(s2);			//pretty print
+//		
+//		std::cout << "\n\n*** stringify ***\n" << s << std::endl;
+//		std::cout << "\n\n*** pretty 1 ***\n" << s2 << std::endl;
+//		
+//		delete doc;
+//		delete doc_root;
+//
+//	} catch (jsonP_exception &ex) {
+//		std::cout << "parse exception caught: " << ex.what() << std::endl;
+//		delete doc;
+//		delete doc_root;
+//	}
 	
 	
 	//sample of creating json object and then accessing its elements
-	element_object *e_object = nullptr;
-	try {
-		e_object = new element_object{};
-		populate_object(e_object);
-		print_object(e_object);
-		
-		std::string s;
-		e_object->stringify_pretty(s);
-		std::cout << "\n\n*** pretty 2 ***\n" << s << std::endl;
-		delete e_object;
-	} catch (jsonP_exception &ex) {
-		std::cout << "json exception caught: " << ex.what() << std::endl;
-		delete e_object;
-	}
+//	element_object *e_object = nullptr;
+//	try {
+//		e_object = new element_object{};
+//		populate_object(e_object);
+//		print_object(e_object);
+//		
+//		std::string s;
+//		e_object->stringify_pretty(s);
+//		std::cout << "\n\n*** pretty 2 ***\n" << s << std::endl;
+//		delete e_object;
+//	} catch (jsonP_exception &ex) {
+//		std::cout << "json exception caught: " << ex.what() << std::endl;
+//		delete e_object;
+//	}
 	
 	
 	//sample parsing json text files
-	jsonP_doc *parse_doc = nullptr;
-	element_object *parse_object = nullptr;
-		
-	try {
-		std::string json_pretty;
-		
-		{
-			std::cout << "\n\n*** opening text file ***\n";
-			std::string json;
-//			parse_file("../samples/webapp.json", json);
-			parse_file("../samples/med.json", json);
-			std::cout << "*** parsing text file with jsonP ***\n";
+//	jsonP_doc *parse_doc = nullptr;
+//	element_object *parse_object = nullptr;
+//		
+//	try {
+//		std::string json_pretty;
+//		
+//		{
+//			std::cout << "\n\n*** opening text file ***\n";
+//			std::string json;
+//			parse_file("/Users/user1/Downloads/large.json", json);
+////			parse_file("../samples/med.json", json);
+//			std::cout << "*** parsing text file with jsonP ***\n";
+//
+//			long l0 = clock2::now().time_since_epoch().count();
+//			auto t0 = clock2::now();
+//
+//			jsonP_parser parser{json};
+//			parse_doc = parser.parse();
+//
+//			long l1 = clock2::now().time_since_epoch().count();
+//			auto t1 = clock2::now();
+//			std::cout << "DONE parsing, elapsed time: " << (l1-l0) << ", " << mil(t1-t0).count() << "ms\n";
+//		}
+//		
+//		parse_object = parse_doc->get_object();
+//		std::cout << "*** calling pretty stringify ***\n";
+//		parse_object->stringify_pretty(json_pretty);
+////parse_object->stringify(json_pretty);
+//		
+//		delete parse_doc;
+//		delete parse_object;
+//		
+//		std::cout << "*** contents of json ***\n";
+//		std::cout << json_pretty << std::endl;
+//	} catch (jsonP_exception &ex) {
+//		std::cout << "Parse exception parsing text file: " << ex.what() << std::endl;
+//		delete parse_doc;
+//		delete parse_object;
+//	}
 
-			long l0 = clock2::now().time_since_epoch().count();
-			auto t0 = clock2::now();
 
-			jsonP_parser parser{json};
-			parse_doc = parser.parse();
+	// sample parsing json file using the buffer parser
+//	jsonP_buffer_parser buf_parser{"/Users/user1/Downloads/large.json", 8192};
+	jsonP_buffer_parser buf_parser{"//Users/user1/udemy/CPP/UdemyCPP/jsonP_dyn_drvr/samples/webapp.json", 1000};
+	auto buf_t0 = clock2::now();
+	jsonP_doc *doc_buf = buf_parser.parse();
+	auto buf_t1 = clock2::now();
+	std::string buf_s;
+	element_object *buf_obj = doc_buf->get_object();
+	buf_obj->stringify_pretty(buf_s);
+	std::cout << "\n\n" << doc_buf->get_type() << "\n\n";
+	std::cout << buf_s << std::endl;
 
-			long l1 = clock2::now().time_since_epoch().count();
-			auto t1 = clock2::now();
-			std::cout << "DONE parsing, elapsed time: " << (l1-l0) << ", " << mil(t1-t0).count() << "ms\n";
-		}
-		
-		parse_object = parse_doc->get_object();
-		std::cout << "*** calling pretty stringify ***\n";
-		parse_object->stringify_pretty(json_pretty);
-		
-		delete parse_doc;
-		delete parse_object;
-		
-		std::cout << "*** contents of json ***\n";
-		std::cout << json_pretty << std::endl;
-	} catch (jsonP_exception &ex) {
-		std::cout << "Parse exception parsing text file: " << ex.what() << std::endl;
-		delete parse_doc;
-		delete parse_object;
-	}
+	std::cout << "buffer parser parse time: " << mil(buf_t1-buf_t0).count() << "/ms" << std::endl;
+	
+	delete buf_obj;
+	delete doc_buf;
  
 	return 0;
 }
@@ -182,16 +203,32 @@ void print_object(element_object *doc)
 
 void parse_file(std::string f, std::string &json)
 {
-	std::ifstream in_file{f};
+//	std::ifstream in_file{f};
 	std::string line;
 	
-	if (!in_file) {
+//	if (!in_file) {
+//		std::cerr << "File failed to open" << std::endl;
+//		return;
+//	}
+//	
+//	while (std::getline(in_file, line))
+//		json += line;
+		
+	
+	FILE *fp = std::fopen(f.c_str(), "r");
+	
+	if (fp == NULL) {
 		std::cerr << "File failed to open" << std::endl;
 		return;
 	}
 	
-	while (std::getline(in_file, line))
-		json += line;
+	char buf[4096];
+	
+	while (fread((void *)buf, sizeof(char), 4095, fp) > 0) {
+		json += buf;
+	}
+	
+	fclose(fp);
 	
 	std::cout << f << ", file size: " << json.length() << std::endl;
 }
